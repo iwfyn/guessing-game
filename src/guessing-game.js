a1bf432ca7fb5476feba8eac222e6ddfde0973e0
Dzima Bykau <bykovdmitry3@gmail.com>
2023-10-29 23:26:17 +0300

    feat: complete guessing game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minRange = 0;
+        this.maxRange = 0;
+      }
 
-    setRange(min, max) {
-
-    }
+      setRange(min, max) {
+        this.minRange = min;
+        this.maxRange = max;
+      }
 
     guess() {
-
+        return Math.round(this.minRange + (this.maxRange - this.minRange) / 2);
     }
 
     lower() {
-
+        this.maxRange = this.guess();
     }
 
     greater() {
-
+        this.minRange = this.guess();
     }
 }
 
